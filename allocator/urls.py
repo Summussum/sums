8d69cc7dde4e6b354b5ca8ecc70af35d4d5d17ca
Summussum <jsvend.mcquay@gmail.com>
2024-08-21 06:20:47 -0700

    Allocate API

--- allocator/urls.py
+++ allocator/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="allocator_index"),
     path("create_budget_category/", views.create_budget_category, name="create_budget_category"),
     path("display_active_budget/", views.display_active_budget, name="display_active_budget"),
+    path("<slug:category_name>", views.allocate, name="allocator"),
+    path("reset/<slug:category_name>", views.reset, name="budget_display_reset"),
 ]
--- allocator/views.py
+++ allocator/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from render_block import render_block_to_string
 from django.db import models
 from django.template import loader
 from sums.models import Budgets, Users
@@ -36,4 +36,52 @@ def display_active_budget(request):
     template = loader.get_template("Allocate/budget_display.html")
     return HttpResponse(template.render(context, request))
 
+def allocate(request, category_name):
+    if request.method == 'GET':
+        context = {"category_name": category_name}
+        html = render_block_to_string("Allocate/allocator.html", "editor", context=context, request=request)
+        # html = ""
+        return HttpResponse(html)
+
+    elif request.method == 'POST':
+
+        user = Users.objects.get(username="test")
+        new_category_name = request.POST.get("category_name")
+        amount = request.POST.get("amount")
+
+        if Budgets.objects.filter(category_name=new_category_name).exists() and new_category_name != category_name:
+            response = render(request, "Allocate/category_already_exists.html")
+            response["HX-Retarget"] = "#name_in_use"
+            return response
+        else:
+            if request.POST.get("annual"):
+                 new_budget = Budgets(username=user,category_name=new_category_name,annual_budget=amount)
+            else:
+                new_budget = Budgets(username=user,category_name=new_category_name,monthly_budget=amount)
+            
+            instance = Budgets.objects.filter(category_name=category_name)
+            instance.delete()
+            new_budget.save()
+            return display_active_budget(request)
+
+
+    elif request.method == 'DELETE':
+        instance = Budgets.objects.filter(category_name=category_name)
+        budget = instance.first()
+        if budget.monthly_budget:
+            amount = budget.monthly_budget
+        elif budget.annual_budget:
+            amount = budget.annual_budget
+        else:
+            amount = 0
+        instance.delete()
+        html = f"<tr><td>Category named {category_name}: ${amount} has successfully been deleted.</td></tr>"
+        return HttpResponse(html)
+
+def reset(request, category_name):
+    return display_active_budget(request)
+    """budget = Budgets.objects.filter(category_name=category_name).first()
+    context = {"category_name": budget.category_name, "monthly_budget": budget.monthly_budget, "annual_budget": budget.annual_budget}
+    html = render_block_to_string("Allocate/allocator.html", "data", context=context, request=request)
+    return HttpResponse(html)"""
 
